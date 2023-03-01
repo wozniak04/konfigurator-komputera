08946fe915ebd4769098ed6629a0aefa813dfc4b
michuu17 <92311009+michuu17@users.noreply.github.com>
2023-03-01 09:58:32 +0100

    Update database.sql

--- konfigurator_kompow/database.sql
+++ konfigurator_kompow/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 01 Mar 2023, 09:38
+-- Czas generowania: 01 Mar 2023, 09:58
 -- Wersja serwera: 10.4.27-MariaDB
 -- Wersja PHP: 8.2.0
 
@@ -28,10 +28,38 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
+  `id` int(200) NOT NULL,
   `login` varchar(16) NOT NULL,
   `password` varchar(16) NOT NULL,
   `email` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `email`) VALUES
+(1, 'admin', 'admin', 'admin@admin.pl');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
