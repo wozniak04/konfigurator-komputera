a13745778a743b71e4a095d055ecb4fb98f56b56
michuu17 <michal.ruc@wp.pl>
2023-03-03 16:20:17 +0100

    przełączenie między stronami

--- konfigurator_kompow/src/App.tsx
+++ konfigurator_kompow/src/App.tsx
@@ -1,4 +1,7 @@
+import { Route, Routes } from "react-router";
 import Footer from "./components/Footer";
+import Konfig from "./components/Konfig";
+import Logowanie from "./components/Logowanie";
 import Menus from "./components/Menus";
 import Mid from "./components/Mid";
 
@@ -6,9 +9,16 @@ function App() {
 
   return (
     <>
-      <Menus />
-      <Mid />
-      <Footer />
+      <Routes>
+        <Route path="/Logowanie" element={<Logowanie />} />
+        <Route path="/" element={<>
+          <Menus />
+          <Mid />
+          <Footer />
+        </>}>
+        </Route>
+        <Route path="/Konfig" element={<Konfig src={""} opis={""} />}/>
+      </Routes>
     </>
   );
 }
--- konfigurator_kompow/src/components/Menus.tsx
+++ konfigurator_kompow/src/components/Menus.tsx
@@ -3,27 +3,9 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-import { useNavigate } from 'react-router';
-import { BrowserRouter as Router,
-Routes,
-Route,
-Link } from 'react-router-dom';
-
-import Konfig from './Konfig';
-import Logowanie from './Logowanie';
-
-
-
-
-// const konfigurator = () => {
-//   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//     <React.StrictMode>
-//       <Konfig src={''} opis={''} />
-//     </React.StrictMode>
-//   )
-// }
+import {
+  Link
+} from 'react-router-dom';
 
 const Menus = () => {
  
@@ -32,9 +14,7 @@ const Menus = () => {
 
 export default Menus;
 
-
 const MenuLayout = () => (
-<>
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
@@ -42,17 +22,10 @@ const MenuLayout = () => (
             O nas
           </Typography>
           <Typography  variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <Link to="/Konfigurator_Kompow">Konfigurator Komputerów</Link>
+          <Link to="/Konfig">Konfigurator komputerów</Link>
           </Typography>
-          <Button color="inherit"><Link to="/logowanie">Logowanie</Link></Button>
+          <Button color="inherit"><Link to={"/Logowanie"}>Logowanie</Link></Button>
         </Toolbar>
       </AppBar>
     </Box>
-
-    <Routes>
-      <Route path="/logowanie" element={<Logowanie/>}/>
-      <Route path="/Konfigurator_Kompow" element={<Konfig src="#" opis=""/>}/>
-    </Routes>
-</>
-
 )
