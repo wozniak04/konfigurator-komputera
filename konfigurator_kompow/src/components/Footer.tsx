c0cb7095108afd2aece76c93e5e277401b4932b2
JakubBlazk <91121129+JakubBlazk@users.noreply.github.com>
2023-03-14 12:32:31 +0100

    .

--- konfigurator_kompow/src/components/Footer.tsx
+++ konfigurator_kompow/src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import '../style/Footer.scss';
+import CookieConsent, { Cookies, resetCookieConsentValue } from "react-cookie-consent"
 const Footer = () => {
     return <FooterLayout />
 }
@@ -12,5 +13,9 @@ const FooterLayout = () => (
         Telefon: 123-456-789
         <h4><a href="">Warunki</a></h4>
         <h4><a href="">Pomoc</a></h4>
+        <CookieConsent location="bottom"buttonText="Sure man!!"cookieName="myAwesomeCookieName2"style={{ background: "#2B373B" }}buttonStyle={{ color: "#4e503b", fontSize: "13px" }}expires={150}>
+            This website uses cookies to enhance the user experience.{" "}
+            <span style={{ fontSize: "10px" }}>This bit of text is smaller :O</span>
+        </CookieConsent>
     </div>
 )
